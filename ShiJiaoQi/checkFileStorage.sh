38f7b4d57c081508d94a13acf20ea684b2f6e94a
Neod0Matrix <1054465075@qq.com>
2018-09-16 21:48:13 +0800

    创建仓库雏形, V0.8.81

---
+++ ShiJiaoQi/checkFileStorage.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+# check /ShiJiaoQi directory about file storage files content
+# before using please use root permission
+# this script use absolute path, it can run in everywhere
+# code by </MATRIX>@Neod Anderjon
+
+echo "Welcome to use </MATRIX> shell script"
+echo "code by </MATRIX>@Neod Anderjon"
+
+echo "==========>Check order_file.d directory content:"
+ls -l /ShiJiaoQi/order_file.d
+echo "==========>Check order_file_usb.d directory content:"
+ls -l /ShiJiaoQi/order_file_usb.d
+echo "==========>Check finished"
+
+# code by </MATRIX>@Neod Anderjon
